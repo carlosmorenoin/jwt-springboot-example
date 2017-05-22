bf9fbd43afb1bbf0fd69c39cad95edcc56ea0631
Carlos Moreno <carlos.moreno@netcentric.biz>
2017-05-22 10:13:22 +0200

    Simple example completed

--- src/main/java/org/example/jwt/Protected.java
+++ src/main/java/org/example/jwt/Protected.java
@@ -2,6 +2,8 @@ package org.example.jwt;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,12 +22,7 @@ public class Protected {
         if (!validateToken(request)) {
             throw new ForbiddenException();
         }
-        return "PROTECTED RESOURCE";
-    }
-
-
-    private String retrieveJWT(HttpServletRequest request) {
-        return request.getHeader("access_token");
+        return "<h2>Welcome to the PROTECTED RESOURCE</h2>";
     }
 
     private boolean validateToken(final HttpServletRequest request) {
@@ -35,8 +32,12 @@ public class Protected {
         }
         Claims claims = Jwts.parser().setSigningKey(Secret.getKey()).parseClaimsJws(jwt).getBody();
 
-        return "EXTERNAL".equals(claims.get("externalServiceId"))
+        return "EXTERNAL_ID".equals(claims.get("externalServiceId"))
                 && "demo".equals(claims.getSubject());
+    }
 
+    private String retrieveJWT(HttpServletRequest request) {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        return StringUtils.substringAfter(authHeader, "Bearer ");
     }
 }
